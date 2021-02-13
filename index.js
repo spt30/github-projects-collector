bd81251c1316cf2537862006963c638ef060034b
Тощилин Сергей <31798669+spt30@users.noreply.github.com>
2021-02-13 19:02:38 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+let count = 0;
+
+const res = [...document.getElementsByClassName('project-column')].reverse().map(column => {
+    const columnName = column.getElementsByClassName('js-project-column-name')[0].innerText;
+    const cards = [...column.getElementsByClassName('issue-card')];
+
+    const links = cards.map(card => {
+        ++count;
+        const linkTag = card.getElementsByClassName('js-project-card-issue-link')[0];
+
+        const link = window.location.origin + linkTag.getAttribute('href');
+        const text = linkTag.innerText;
+        let executor = '';
+
+        if (card.getElementsByClassName('avatar-user')[0]) {
+            executor = card.getElementsByClassName('avatar-user')[0].getAttribute('alt');
+        }
+
+        return [text, executor, columnName, link].join('\t');
+    });
+
+    return links;
+}).flat().join('\n')
+
+copy(res);
+
+console.log('Всего задач:', count);
